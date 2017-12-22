b118bdc679df69a4fabb08d852727f8b0c32bb30
Adam Koch <adam@dogtired.org>
2017-12-21 22:19:26 -0600

    gulp-gh-pages added

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+    ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function () {
+    return gulp.src('./_site/**/*')
+        .pipe(ghPages({
+            'branch': 'master'
+        }));
+});
